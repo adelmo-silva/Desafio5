aabae4956eadd11bc4a6fc1fe5f22639159706d7
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-22 22:31:45 -0300

    Única versão

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿{
+    Console.Write("Jogador (1), informe um valor: ");
+    double valor1 = double.Parse(Console.ReadLine());
+    Console.Write("Jogador (2), informe um valor: ");
+    double valor2 = double.Parse(Console.ReadLine());
+    int quanttent = 1;
+
+    if(valor1 != valor2)
+    {
+        while(valor1 != valor2)
+        {
+            Console.WriteLine("Tentativas: " + quanttent);
+            Console.WriteLine("O valor está incorreto!");
+            Console.Write("Jogador (2), informe um valor novamente:");
+            valor2 = double.Parse(Console.ReadLine());
+            quanttent = quanttent + 1;
+        }
+    }
+    
+    Console.WriteLine("Tentativas: " + quanttent);
+    Console.Write("O valor informado está correto, Parabéns!");
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+Construa um algoritmo para um mini game, no qual o jogador(1) entra com um número e o jogador(2) deve adivinhar o número informado.
+
+Regras:
+1) O algoritmo deve pedir para o jogador(1) informar um valor.
+
+2) O algoritmo deve pedir para jogador(2) informar um valor numérico igual ao que o jogador(1) informou.
+
+a) Se o jogador(2) informar um valor igual, o algoritmo deve exibir uma mensagem de sucesso e a quantidade de tentativas.
+b)Se o valor for diferente o sistema deve informar que o valor está incorreto, exibir a quantidade de tentativas e pedir um valor novamente.
