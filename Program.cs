1426b1984cdfb0499d182682e15f29ad3f30dfe5
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-23 01:10:20 -0300

    Versão adicional

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
 ﻿{
-    Console.Write("Jogador (1), informe um valor: ");
+    Console.WriteLine("O objetivo do jogo é adivinhar o número informado pelo primeiro jogador!");
+    Console.Write("Jogador (1), informe um valor de 0 a 9: ");
     double valor1 = double.Parse(Console.ReadLine());
-    Console.Write("Jogador (2), informe um valor: ");
+    Console.Clear();
+
+    while(valor1 < 0 || valor1 > 9)
+    {
+        Console.WriteLine("Error!");
+        Console.Write("Informe um valor entre 0 e 9 por favor!:");
+        valor1 = double.Parse(Console.ReadLine());
+        Console.Clear();
+    }
+        
+    Console.Clear();
+    Console.Write("Jogador (2), informe um valor de 0 a 9: ");
     double valor2 = double.Parse(Console.ReadLine());
+    Console.Clear();
     int quanttent = 1;
 
-    if(valor1 != valor2)
+    while(valor2 < 0 || valor2 > 9)
     {
-        while(valor1 != valor2)
-        {
-            Console.WriteLine("Tentativas: " + quanttent);
-            Console.WriteLine("O valor está incorreto!");
-            Console.Write("Jogador (2), informe um valor novamente:");
-            valor2 = double.Parse(Console.ReadLine());
-            quanttent = quanttent + 1;
-        }
+        Console.WriteLine("Error!");
+        Console.Write("Informe um valor de 0 a 9 por favor!:");
+        valor2 = double.Parse(Console.ReadLine());
+        Console.Clear();
+    }
+
+    while(valor2 != valor1)
+    {
+        Console.WriteLine("Tentativas: " + quanttent);
+        Console.WriteLine("O valor está incorreto!");
+        Console.Write("Jogador (2), informe um valor novamente:");
+        valor2 = double.Parse(Console.ReadLine());
+        quanttent = quanttent + 1;
+        Console.Clear();
     }
     
+    Console.Clear();
     Console.WriteLine("Tentativas: " + quanttent);
     Console.Write("O valor informado está correto, Parabéns!");
 }
